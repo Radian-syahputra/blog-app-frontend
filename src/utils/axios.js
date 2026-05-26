6b74ee669d0255f5821001667fab786fa0d6513f
Radian-syahputra <driftwear26@gmail.com>
2026-05-26 08:44:09 +0700

    update baseURL to Render

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://blog-app-backend-production-ed90.up.railway.app/api',
+    baseURL: 'https://blog-app-backend-i556.onrender.com/api',
     withCredentials: true
 })
 
